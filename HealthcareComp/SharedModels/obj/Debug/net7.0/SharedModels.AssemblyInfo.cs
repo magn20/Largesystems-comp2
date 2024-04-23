aa54dc6f9d97818e5b2697c5427082780c611715
ChristianHesberg <chri59kx@easv365.dk>
2024-04-23 15:28:45 +0200

    feat: ci action

--- HealthcareComp/SharedModels/obj/Debug/net7.0/SharedModels.AssemblyInfo.cs
+++ HealthcareComp/SharedModels/obj/Debug/net7.0/SharedModels.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SharedModels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11aeab07d5683e59317717b51dadb52ed06a3433")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharedModels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharedModels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
